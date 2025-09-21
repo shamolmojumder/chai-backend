1c7c33c70a10761e7e2d2cd1006d9f240fed85fc
Shamol Mojumder <shamolmojumder14@gmail.com>
2025-09-21 23:47:39 +0600

    Writing update controllers for user | Backend with JS 30

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -204,7 +204,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 const changeCurrentPassword = asyncHandler(async (req, res) => {
     const { oldPassword, newPassword } = req.body;
 
-    const user = await User.findById(req.user?.id);
+    const user = await User.findById(req.user?._id);
     const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
     if (!isPasswordCorrect) {
         throw new ApiError(400, "Invalid Old Password")
@@ -222,7 +222,23 @@ const getCurrentUser = asyncHandler(async (req, res) => {
 })
 
 
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    const { fullName, email, } = req.body;
 
+    if (!fullName || !email) {
+        throw new ApiError(400, "All fields required")
+    }
+
+    const user = User.findByIdAndUpdate(req.user?._id,
+        {
+            $set: {
+                fullName,
+                email
+            }
+        }, { new: true }).select("-password")
+
+    return res.status(200).json(new ApiResponse(200, user, "account details successfully"))
+})
 
 
 export {
@@ -231,5 +247,6 @@ export {
     logoutUser,
     refreshAccessToken,
     changeCurrentPassword,
-    getCurrentUser
+    getCurrentUser,
+    updateAccountDetails
 };
